5b9ada6ce6e32a541d69e0e73286dc847422e0b4
odonnellryanc <odonnellryanc@gmail.com>
2015-04-08 13:42:42 -0400

    echo's back client  hostname and client port
    probably not the way you should code haskell!

--- main.hs
+++ main.hs
@@ -1,20 +1,33 @@
-{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+--{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
-import Network.Socket
+import Network
+import System.IO
+
+-- beginning of web server. just listens on that port and serves the test page.
 
 main :: IO ()
 main = do
-	sock <- socket AF_INET Stream 0
-	setSocketOption sock ReuseAddr 1
-	bindSocket sock (SockAddrInet 4242 iNADDR_ANY)
-	listen sock 2
+	-- create the socket
+	sock <- listenOn (PortNumber 8080)
 	loop sock
 
+loop :: Socket -> IO ()
 loop sock = do
-	connection <- accept sock
-	handleConn connection
+	-- accept the connection from the socket
+	-- 
+	(connection, hostName, portNumber) <- accept sock
+	handleConn connection hostName portNumber
 	loop sock
 
-handleConn (sock, _) = do
-	_ <- send sock "Testing!!\n"
-	sClose sock
+handleConn:: Handle -> HostName -> PortNumber -> IO ()
+handleConn connection host port = do
+	hPutStr connection (msg host port) 
+	hFlush  connection
+	hClose  connection
+
+msg :: HostName -> PortNumber -> String
+--msg = "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\n" ++ "\r\n"
+msg host port = "HTTP/1.0 200 OK\r\nContent-Length: " ++ show (length(formatPortAndHost host port)) ++ "\r\n\r\n" ++ formatPortAndHost host port ++ "\r\n"
+
+formatPortAndHost :: HostName -> PortNumber -> String
+formatPortAndHost host port = "Host: " ++ show host ++ " Port: " ++ show port
