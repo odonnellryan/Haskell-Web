68cf92efa8f4855e24920f8c980dd2f5de9fae25
odonnellryanc <odonnellryanc@gmail.com>
2015-04-08 12:55:31 -0400

    using some sockets!

--- main.hs
+++ main.hs
@@ -1,2 +1,20 @@
-import Network
-import Control.Concurrent
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
+import Network.Socket
+
+main :: IO ()
+main = do
+	sock <- socket AF_INET Stream 0
+	setSocketOption sock ReuseAddr 1
+	bindSocket sock (SockAddrInet 4242 iNADDR_ANY)
+	listen sock 2
+	loop sock
+
+loop sock = do
+	connection <- accept sock
+	handleConn connection
+	loop sock
+
+handleConn (sock, _) = do
+	_ <- send sock "Testing!!\n"
+	sClose sock
