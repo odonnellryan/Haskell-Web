5b2c372839f67f417fe043037863835cc768f2f8
odonnellryanc <odonnellryanc@gmail.com>
2015-04-09 10:12:20 -0400

    added some formatting functions for headers

--- main.hs
+++ main.hs
@@ -3,6 +3,11 @@
 import Network
 import System.IO
 
+-- defining our custom types. We derive from Show so that 
+-- we can use show on these types later!
+data ReturnCode = ReturnCode Int String deriving (Show)
+
+
 -- beginning of web server. just listens on that port and serves the test page.
 
 main :: IO ()
@@ -14,23 +19,30 @@ main = do
 loop :: Socket -> IO ()
 loop sock = do
 	-- accept the connection from the socket
-	-- 
 	(connection, hostName, portNumber) <- accept sock
 	handleConn connection hostName portNumber
 	loop sock
 
-handleConn:: Handle -> HostName -> PortNumber -> IO ()
+handleConn :: Handle -> HostName -> PortNumber -> IO ()
 handleConn connection host port = do
 	hPutStr connection (msg host port) 
 	hFlush  connection
 	hClose  connection
 
 msg :: HostName -> PortNumber -> String
---msg = "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\n" ++ "\r\n"
-msg host port = "HTTP/1.0 200 OK\r\nContent-Length: " ++ msgLength ++ "\r\n\r\n" ++ formattedMsg ++ "\r\n"
+msg host port = wrapWithHeaders returnCode body
 	where 
-		formattedMsg = show (formatPortAndHost host port)
-		msgLength = show(length formattedMsg)
-
+		body = show (formatPortAndHost host port)
+		returnCode = ReturnCode 200 "OK"
+		
 formatPortAndHost :: HostName -> PortNumber -> String
 formatPortAndHost host port = "Host: " ++ show host ++ " Port: " ++ show port
+
+-- not sure if this will work, having a moment.
+httpHeaders :: Int -> String -> Int -> [String]
+httpHeaders code status msgLength = ["HTTP/1.0", show code, status, "\r\nContent-Length:", show msgLength, "\r\n\r\n"]
+
+wrapWithHeaders :: ReturnCode -> String -> String
+wrapWithHeaders (ReturnCode code status) body = unwords (httpHeaders code status bLen) ++ body ++ "\r\n"
+	where
+		bLen = length body
