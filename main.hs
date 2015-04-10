c0635448451f43018fed86e13b0ababf11ce939c
odonnellryanc <odonnellryanc@gmail.com>
2015-04-10 10:11:02 -0400

    started adding cases for the different methods
    added cases for the different methods, starting to parse the request
    from the client. added the functions to handle the various methods.

--- main.hs
+++ main.hs
@@ -7,8 +7,7 @@ import System.IO
 -- we can use show on these types later!
 data ReturnCode = ReturnCode Int String deriving (Show)
 
-
--- beginning of web server. just listens on that port and serves the test page.
+-- beginning of web server. just listens on that port and serves a basic page
 
 main :: IO ()
 main = do
@@ -18,31 +17,66 @@ main = do
 
 loop :: Socket -> IO ()
 loop sock = do
-	-- accept the connection from the socket
-	(connection, hostName, portNumber) <- accept sock
-	handleConn connection hostName portNumber
+	-- accept the handle from the socket
+	(handle, _, _) <- accept sock
+	handleConn handle
 	loop sock
 
-handleConn :: Handle -> HostName -> PortNumber -> IO ()
-handleConn connection host port = do
-	hPutStr connection (msg host port) 
-	hFlush  connection
-	hClose  connection
+handleConn :: Handle -> IO ()
+handleConn handle = do
+	-- we get the request from the client
+	line <- hGetLine handle
+	-- split the request into a list of params. first is going to be the METHOD.
+	-- this probably works because most(?) times the params sent fromt he client are
+	-- space delimited. i think.
+	let method = words line
+	case head method of
+		-- not sure if this is best, but I guess we'll see!
+		("GET") -> hPutStr handle (handleGet line)
+		("POST") -> hPutStr handle (handlePost line)
+		("PUT") -> hPutStr handle (handlePut line)
+		("DELETE") -> hPutStr handle (handleDelete line)
+		("TRACE") -> hPutStr handle (handleTrace line)
+		("CONNECT") -> hPutStr handle (handleConnect line)
+		_ -> hPutStr handle (handleError line)
+	hFlush  handle
+	hClose  handle
 
-msg :: HostName -> PortNumber -> String
-msg host port = wrapWithHeaders returnCode body
-	where 
-		body = show (formatPortAndHost host port)
+handleGet :: String -> String
+handleGet line = addHeader returnCode body
+	where
+		body = serveFile line
 		returnCode = ReturnCode 200 "OK"
-		
-formatPortAndHost :: HostName -> PortNumber -> String
-formatPortAndHost host port = "Host: " ++ show host ++ " Port: " ++ show port
 
--- not sure if this will work, having a moment.
+handlePost :: String -> String
+handlePost line = "test: " ++ line
+
+handlePut :: String -> String
+handlePut line = "test: " ++ line
+
+handleDelete :: String -> String
+handleDelete line = "test: " ++ line
+
+handleTrace :: String -> String
+handleTrace line = "test: " ++ line
+
+handleConnect :: String -> String
+handleConnect line = "test: " ++ line
+
+handleError :: String -> String
+handleError line = addHeader returnCode body
+	where
+		body = serveFile line
+		returnCode = ReturnCode 200 "OK"
+
+
+serveFile :: String -> String
+serveFile line = line ++ line
+
 httpHeaders :: Int -> String -> Int -> [String]
 httpHeaders code status msgLength = ["HTTP/1.0", show code, status, "\r\nContent-Length:", show msgLength, "\r\n\r\n"]
 
-wrapWithHeaders :: ReturnCode -> String -> String
-wrapWithHeaders (ReturnCode code status) body = unwords (httpHeaders code status bLen) ++ body ++ "\r\n"
+addHeader :: ReturnCode -> String -> String
+addHeader (ReturnCode code status) body = unwords (httpHeaders code status bLen) ++ body ++ "\r\n"
 	where
 		bLen = length body
