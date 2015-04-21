9042c1beac2e660d6072b3bca9ed3425a5462c8b
odonnellryanc <odonnellryanc@gmail.com>
2015-04-21 08:40:56 -0400

    change it up
    still not working correctly! :O

--- main.hs
+++ main.hs
@@ -1,100 +1,53 @@
 --{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
-
-import Network
-import System.IO
-
-
-
--- defining our custom types. We derive from Show so that 
--- we can use show on these types later!
-data ReturnCode = ReturnCode Int String deriving (Show)
-
--- beginning of web server. just listens on that port and serves a basic page
+ -- Echo server program
+module Main where
+import Control.Monad (unless)
+import Network.Socket hiding (recv)
+import qualified Data.ByteString as S
+import Network.Socket.ByteString
 
 main :: IO ()
-main = do
-	-- create the socket
-	sock <- listenOn (PortNumber 8080)
+main = withSocketsDo $ do 
+	addrinfos <- getAddrInfo (Just (defaultHints {addrFlags = [AI_PASSIVE]})) Nothing (Just "8080")
+	let serveraddr = head addrinfos
+	sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+	bindSocket sock (addrAddress serveraddr)
+	listen sock 1
 	loop sock
+	sClose sock
 
 loop :: Socket -> IO ()
 loop sock = do
-	-- accept the handle from the socket
-	(handle, _, _) <- accept sock
-	handleConn handle
+	(conn, _) <- accept sock
+	talk conn
+	sClose conn
 	loop sock
 
--- takes io until the predicate is met, something something monad
-takeUntilCondition :: (a -> Bool) -> [IO a] -> IO [a]
-takeUntilCondition predicate line = do
-	-- basically, checks if the line accepted against the predicate.
-	x <- head line
-	if predicate x
-    	then takeUntilCondition predicate (tail line) >>= \xs -> return (x:xs)
-    	else return []
+talk :: Socket -> IO ()
+talk conn = do 
+	msg <- recv conn 1024
+	print msg
+	unless (S.null msg) $ do
+		m <- build msg conn
+		print m
+		sendAll conn m
+	--m <- testing msg conn
 
-handleConn :: Handle -> IO ()
-handleConn handle = do
-	-- we get the request from the client
-	-- this probably should be using bytestrings but whatever
-	-- update: definitely use bytestrings, otherwise we've crossed the hacky line
-	let l = repeat (hGetLine handle)
-	-- feed until we get the first line that is a carriage return
-	l2 <- takeUntilCondition (/= "\r") l
-	print l2
-	--print l
-	-- split the request into a list of params. first is going to be the METHOD.
-	let request = words (head l2)
-	print (tail l2)
-	let line = head l2
-	case head request of
-		-- not sure if this is best, but I guess we'll see!
-		("GET") -> hPutStr handle (handleGet line)
-		("POST") -> hPutStr handle (handlePost line)
-		("PUT") -> hPutStr handle (handlePut line)
-		("DELETE") -> hPutStr handle (handleDelete line)
-		("HEAD") -> hPutStr handle (handleHead line)
-		-- handle default case
-		_ -> hPutStr handle (handleError "" (ReturnCode 443 "Invalid method."))
-	hFlush handle
-	hClose handle
+--build :: IO S.ByteString -> Socket -> IO S.ByteString
+build :: S.ByteString -> Socket -> IO S.ByteString
+build msg conn = do
+    m <- recv conn 1024
+    print m
+    if S.null m then return msg
+    else build (S.append msg m) conn
 
-
-
--- begin handlers
-
-handleGet :: String -> String
-handleGet line = addStatus returnCode body
-	where
-		body = serveFile line
-		returnCode = ReturnCode 200 "OK"
-
-handlePost :: String -> String
-handlePost line = "test: " ++ line
-
-handlePut :: String -> String
-handlePut line = "test: " ++ line
-
-handleDelete :: String -> String
-handleDelete line = "test: " ++ line
-
-handleHead :: String -> String
-handleHead line = "test: " ++ line
-
-handleError :: String -> ReturnCode -> String
-handleError body code = addStatus code body
-
+--serveFile :: String -> String
+--serveFile line = line
 --
-
-
-
-serveFile :: String -> String
-serveFile line = line
-
-statusLine :: Int -> String -> Int -> [String]
-statusLine code status msgLength = ["HTTP/1.0", show code, status, "\r\nContent-Length:", show msgLength, "\r\n\r\n"]
-
-addStatus :: ReturnCode -> String -> String
-addStatus (ReturnCode code status) body = unwords (statusLine code status bLen) ++ body ++ "\r\n"
-	where
-		bLen = length body
+--statusLine :: Int -> String -> Int -> [String]
+--statusLine code status msgLength = ["HTTP/1.0", show code, status, "\r\nContent-Length:", show msgLength, "\r\n\r\n"]
+--
+--addStatus :: ReturnCode -> String -> String
+--addStatus (ReturnCode code status) body = unwords (statusLine code status bLen) ++ body ++ "\r\n"
+--	where
+--		bLen = length body
