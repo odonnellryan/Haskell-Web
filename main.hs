efcbc446f8ebd2f7042ac1040b3c0c5f3854223e
odonnellryanc <odonnellryanc@gmail.com>
2015-04-21 08:50:53 -0400

    Hmm

--- main.hs
+++ main.hs
@@ -41,8 +41,61 @@ takeUntilCondition predicate line = do
     	then takeUntilCondition predicate (tail line) >>= \xs -> return (x:xs)
     	else return []
 
+<<<<<<< HEAD
 --serveFile :: String -> String
 --serveFile line = line
+=======
+handleConn :: Handle -> IO ()
+handleConn handle = do
+	-- we get the request from the client
+	-- this probably should be using bytestrings but whatever
+	-- update: definitely use bytestrings, otherwise we've crossed the hacky line
+	let l = repeat (hGetLine handle)
+	-- feed until we get the first line that is a carriage return
+	l2 <- takeUntilCondition (/= "\r") l
+	print l2
+	-- split the request into a list of params. first is going to be the METHOD.
+	let request = words (head l2)
+	print (tail l2)
+	let line = head l2
+	case head request of
+		-- not sure if this is best, but I guess we'll see!
+		("GET") -> hPutStr handle (handleGet line)
+		("POST") -> hPutStr handle (handlePost line)
+		("PUT") -> hPutStr handle (handlePut line)
+		("DELETE") -> hPutStr handle (handleDelete line)
+		("HEAD") -> hPutStr handle (handleHead line)
+		-- handle default case
+		_ -> hPutStr handle (handleError "" (ReturnCode 443 "Invalid method."))
+	hFlush handle
+	hClose handle
+
+
+
+-- begin handlers
+
+handleGet :: String -> String
+handleGet line = addStatus returnCode body
+	where
+		body = serveFile line
+		returnCode = ReturnCode 200 "OK"
+
+handlePost :: String -> String
+handlePost line = "test: " ++ line
+
+handlePut :: String -> String
+handlePut line = "test: " ++ line
+
+handleDelete :: String -> String
+handleDelete line = "test: " ++ line
+
+handleHead :: String -> String
+handleHead line = "test: " ++ line
+
+handleError :: String -> ReturnCode -> String
+handleError body code = addStatus code body
+
+>>>>>>> parent of 342dabc... Oops
 --
 --statusLine :: Int -> String -> Int -> [String]
 --statusLine code status msgLength = ["HTTP/1.0", show code, status, "\r\nContent-Length:", show msgLength, "\r\n\r\n"]
